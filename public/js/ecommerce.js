e2ef1e6e338eaab355e97c2e5d0dd3ada3083bef
Aleaxo21 <jassamyn.oliva@gmail.com>
2024-02-18 18:57:39 +0800

    changes

--- public/js/ecommerce.js
+++ public/js/ecommerce.js
@@ -1,6 +1,23 @@
 function loadProductData(){
     var request = new XMLHttpRequest();
-
     var productArray=[];
-    request.open("GET","/")
+    
+    request.open("GET","http://localhost:8080/view", true);
+    
+    request.onload =function(){
+        productArray = JSON.parse(request.responseText);
+    }
+    request.send();
+    //<div id ="dynamicProductDataList">
+};
+
+function insertdynamicProducts(productArray){
+    let newContent = "<table><tr>";
+
+    for (let i =0; i < productArray.length; i++){
+        console.log(productArray[i]);
+
+        newContent+=
+            "<td>"
+    }
 }
